cdac324b227eea2daba36a4b95c631bccd15ac95
Anwesh43 <anweshthecool0@gmail.com>
2017-06-12 04:22:52 +0530

    using colorsliderlist in app

--- app/src/main/java/com/anwesome/ui/colorsliderlistdemo/MainActivity.java
+++ app/src/main/java/com/anwesome/ui/colorsliderlistdemo/MainActivity.java
@@ -1,7 +1,11 @@
 package com.anwesome.ui.colorsliderlistdemo;
 
+import android.graphics.Color;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import com.anwesome.ui.colorsliderlist.ColorSliderList;
+import com.anwesome.ui.colorsliderlist.ColorSliderView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +13,11 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ColorSliderList colorSliderList = new ColorSliderList(this);
+        String colors[] = {"#009688","#FFA726","#00E676","#F4511E","#f44336"};
+        for(String color:colors) {
+            colorSliderList.addView(Color.parseColor(color));
+        }
+        colorSliderList.show();
     }
 }
